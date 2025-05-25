47c588495c1027df49b1f529e106d296dce11946
damianstelmach1985 <105154983+damianstelmach1985@users.noreply.github.com>
2025-05-25 11:19:47 +0200

    Create blocked_ip_update.sh

---
+++ blocked_ip_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "start ip update"
+
+location update
+
+{
+  echo "create geoblock hash:net family inet -exist"
+  echo "flush geoblock"
+  for cc in RU AR; do
+    location list-networks-by-cc --family=ipv4 $cc | while read ip; do
+      echo "add geoblock $ip"
+    done
+  done
+} > /usr/local/sbin/blocked_ip_list
+
+ipset restore < /usr/local/sbin/blocked_ip_list
+
+echo "end ip update"
