cf0f75351e97d8a1f79fcd1961a94a0723949bff
damianstelmach1985 <105154983+damianstelmach1985@users.noreply.github.com>
2025-05-25 11:19:10 +0200

    Create blocked_ip.sh

---
+++ blocked_ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+{
+  echo "create geoblock hash:net family inet"
+  for cc in RU AR; do
+    location list-networks-by-cc --family=ipv4 $cc | while read ip; do
+      echo "add geoblock $ip"
+    done
+  done
+} > /usr/local/sbin/blocked_ip_list
+
+ipset restore < /usr/local/sbin/blocked_ip_list
+
+ipset list geoblock > lista
+
+head -10 lista
